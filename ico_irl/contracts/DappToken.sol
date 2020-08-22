8ec2ffc678dc0e86584f140dda5dedaac98dd181
akashbhardwaj04 <akkiworkspace@gmail.com>
2020-08-22 23:08:45 +0530

    updated dappTokens by making them pausable

--- ico_irl/contracts/DappToken.sol
+++ ico_irl/contracts/DappToken.sol
@@ -1,10 +1,9 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.5.16 <0.6.9;
+pragma solidity 0.4.24;
 
-import "openzeppelin-solidity/contracts/token/ERC20/DetailedERC20.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/PausableToken.sol";
 
-contract DappToken is StandardToken, DetailedERC20 {
+contract DappToken is MintableToken, PausableToken, DetailedERC20 {
     constructor(string _name, string _symbol, uint8 _decimals)
         DetailedERC20(_name, _symbol, _decimals)
         public

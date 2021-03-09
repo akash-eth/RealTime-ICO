70175e286cab4068f2e177df54c5c724f8e128c4
Akash Bhardwaj <akkiworkspace@gmail.com>
2021-03-10 00:44:03 +0530

    tested crowdsale

--- ico_irl/test/DappTokenCrowdsale.test.js
+++ ico_irl/test/DappTokenCrowdsale.test.js
@@ -1,8 +1,10 @@
 //const { assert } = require("chai")
 
+const BigNumber = web3.BigNumber;
 const DappToken = artifacts.require("DappToken")
 const DappTokenCrowdsale = artifacts.require("DappTokenCrowdsale")
 require("chai")
+    .use(require('chai-bignumber')(BigNumber))
     .should()
 
 contract(DappTokenCrowdsale, function([_, wallet])  {       // here first arg is "_": means the deployer. And second arg is the wallet
@@ -25,11 +27,31 @@ contract(DappTokenCrowdsale, function([_, wallet])  {       // here first arg is
     this.wallet = wallet;
 
     //deploying crowdsale:
-    this.token = await DappTokenCrowdsale.new(
+    this.crowdsale = await DappTokenCrowdsale.new(
         this.rate,
         this.wallet,
         this.token.address  // calling the address directly, instead of: intializing like: this.token = ""; on line 26
     )
+    
+    
+
+})
+
+describe("crowdsale", function() {
+    it("tracks the token", async function() {
+        const token = await this.crowdsale.token()
+        token.should.equal(this.token.address)
+    })
+
+    it("tracks the rate", async function() {
+        const rate = await this.crowdsale.rate()
+        rate.should.be.bignumber.equal(this.rate)
+    })
+
+    it("tracks the wallet", async function() {
+        const wallet = await this.crowdsale.wallet()
+        wallet.should.equal(this.wallet)
+    })
 })
     
 

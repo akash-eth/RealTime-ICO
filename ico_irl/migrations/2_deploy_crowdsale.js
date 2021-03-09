e82fbb0434a0f9239820cf4115e5b8b00962731b
Akash Bhardwaj <akkiworkspace@gmail.com>
2021-03-09 09:58:56 +0530

    reinitialized

--- ico_irl/migrations/2_deploy_crowdsale.js
+++
@@ -1,9 +0,0 @@
-const DappToken = artifacts.require("./DappToken.sol");
-
-module.exports = function(deployer) {
-    const _name = "Dapp Token";
-    const _symbol = "DAPP";
-    const _decimals = 18;
-
-    deployer.deploy(DappToken, _name, _symbol, _decimals);
-};

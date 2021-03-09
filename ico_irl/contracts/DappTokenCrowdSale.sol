061336672d6eb092fee157b55f63646899497c41
Akash Bhardwaj <akkiworkspace@gmail.com>
2021-03-09 16:11:11 +0530

    crowdsale fallback error

--- ico_irl/contracts/DappTokenCrowdSale.sol
+++ ico_irl/contracts/DappTokenCrowdSale.sol
@@ -3,3 +3,9 @@
 pragma solidity >=0.5.0 < 0.8.0;
 
 import "@openzeppelin/contracts/crowdsale/Crowdsale.sol";
+
+contract DappTokenCrowdSale is Crowdsale {
+    constructor (uint _rate, address payable _wallet, IERC20 _token) Crowdsale(_rate, _wallet, _token) public {
+
+    }
+}

b5b2f8d2a6d64d5f2e02b72f96c7a6c1e92da18d
Akash Bhardwaj <akkiworkspace@gmail.com>
2021-03-11 00:02:21 +0530

    Refundable Crowdsale

--- ico_irl/contracts/DappTokenCrowdSale.sol
+++ ico_irl/contracts/DappTokenCrowdSale.sol
@@ -8,8 +8,9 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/crowdsale/validation/CappedCrowdsale.sol";
 import "@openzeppelin/contracts/crowdsale/validation/TimedCrowdsale.sol";
 import "@openzeppelin/contracts/crowdsale/validation/WhitelistCrowdsale.sol";
+import "@openzeppelin/contracts/crowdsale/distribution/RefundableCrowdsale.sol";
 
-contract DappTokenCrowdsale is Crowdsale, MintedCrowdsale, CappedCrowdsale, TimedCrowdsale, WhitelistCrowdsale {
+contract DappTokenCrowdsale is Crowdsale, MintedCrowdsale, CappedCrowdsale, TimedCrowdsale, WhitelistCrowdsale, RefundableCrowdsale {
 
     //setting up the limit/cap:
     uint256 investorMinCap = 20000000000000000000;
@@ -22,14 +23,16 @@ contract DappTokenCrowdsale is Crowdsale, MintedCrowdsale, CappedCrowdsale, Time
         IERC20 _token,
         uint256 _cap,
         uint256 _openingTime,   //this will unix time in form of sec!!
-        uint256 _closingTime    
+        uint256 _closingTime,
+        uint256 _goal       // setting a goal to be met during fundraising. If not, refund all the fund back to investors !!    
         ) 
         
         Crowdsale (_rate, _wallet, _token)
         CappedCrowdsale(_cap)
-        TimedCrowdsale(_openingTime, _closingTime)  
+        TimedCrowdsale(_openingTime, _closingTime) 
+        RefundableCrowdsale(_goal) 
         public {
-
+            require(_goal <= _cap);
     }
 
     function getUserContribution(

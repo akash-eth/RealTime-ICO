cd467a3864284b483824374774a301e82399ec19
Akash Bhardwaj <akkiworkspace@gmail.com>
2021-03-10 15:08:46 +0530

    tested crowdsale on test network

--- ico_irl/contracts/DappTokenCrowdSale.sol
+++ ico_irl/contracts/DappTokenCrowdSale.sol
@@ -3,9 +3,10 @@
 pragma solidity >=0.5.0 < 0.8.0;
 
 import "@openzeppelin/contracts/crowdsale/Crowdsale.sol";
+import "@openzeppelin/contracts/crowdsale/emission/MintedCrowdsale.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract DappTokenCrowdsale is Crowdsale {
+contract DappTokenCrowdsale is Crowdsale, MintedCrowdsale {
     constructor (uint _rate, address payable _wallet, IERC20 _token) Crowdsale(_rate, _wallet, _token) public {
 
     }
